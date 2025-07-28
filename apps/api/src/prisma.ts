1211fd6b0041c6ad2a0b31e484e43ffda36114f3
narasena <cursedsena@gmail.com>
2025-07-28 13:16:08 +0700

    fix: auth bugs 2

--- apps/api/src/prisma.ts
+++ apps/api/src/prisma.ts
@@ -1,3 +1,4 @@
-import { PrismaClient } from '../src/prisma-generated/client';``
+import './config';
+import { PrismaClient } from '../src/prisma-generated/client';
 
 export const prisma = new PrismaClient({ log: [ 'info', 'warn', 'error'] });
